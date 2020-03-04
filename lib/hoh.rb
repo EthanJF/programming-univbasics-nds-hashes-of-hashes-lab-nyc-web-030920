445345eec7606f7f9fa079ac513c7f6861e6405d
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-04 16:53:28 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,17 +43,27 @@ def naming_system
   # HoH all the way to Species!
   {
     {
-#   label: "Kingdom",
-#   sub_category: {
-#     label: "Phylum",
-#     sub_category: {
-#       label: "Class",
-sub_category: {
-#   label: "Order"
-# }
-#     }
-#   }
-# }
+  label: "Kingdom",
+  sub_category: {
+    label: "Phylum",
+    sub_category: {
+      label: "Class",
+        sub_category: {
+         label: "Order",
+          sub_category: {
+            label: "Family",
+            sub_category: {
+              label: "Genus",
+              sub_category: {
+                label: "Species",
+                sub_category: nil
+              }
+            }
+          }
+        }
+       }  
+     }
+   }
   }
 
 end
